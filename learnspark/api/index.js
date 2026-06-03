d9468eb868d9f95c214f181a23085254a22d6f10
NonchalantGuy41 <bennoss1010@gmail.com>
2026-06-03 20:53:39 +1000

    Update index.js

--- learnspark/api/index.js
+++ learnspark/api/index.js
@@ -1,2 +1,15 @@
 import handler from "../dist/server/server.js";
-export default handler;
+
+export default async function (req, res) {
+  const url = new URL(req.url, `https://${req.headers.host}`);
+  const request = new Request(url, {
+    method: req.method,
+    headers: req.headers,
+    body: req.method !== "GET" && req.method !== "HEAD" ? req : undefined,
+  });
+  const response = await handler.fetch(request, process.env, {});
+  res.statusCode = response.status;
+  response.headers.forEach((value, key) => res.setHeader(key, value));
+  const body = await response.arrayBuffer();
+  res.end(Buffer.from(body));
+}
